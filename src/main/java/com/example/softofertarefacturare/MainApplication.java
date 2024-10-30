9c2c51060fd69f5ded1e8d22d11ca433645f415e
Teolala98 <94044786+Teolala98@users.noreply.github.com>
2024-10-30 23:56:00 +0200

    modificare comanda

---
+++ src/main/java/com/example/softofertarefacturare/MainApplication.java
@@ -0,0 +1,57 @@
+package com.example.softofertarefacturare;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("/com/example/View/logIn.fxml"));
+        Parent loginRoot = loginLoader.load();
+        Scene loginScene = new Scene(loginRoot);
+
+
+        Stage loginStage = new Stage();
+        loginStage.setTitle("Login");
+        loginStage.setScene(loginScene);
+
+
+        LoginController loginController = loginLoader.getController();
+
+
+        loginController.setOnLoginSuccess(new Runnable() {
+            @Override
+            public void run() {
+                loginStage.close();
+                try {
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/View/profile-view.fxml"));
+                    Parent root = fxmlLoader.load();
+
+                    Scene mainScene = new Scene(root, 900, 1000);
+                    primaryStage.setTitle("Calcul Materiale");
+                    primaryStage.setScene(mainScene);
+
+                    primaryStage.setOnCloseRequest(event -> System.exit(0));
+
+                    primaryStage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+        loginStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
